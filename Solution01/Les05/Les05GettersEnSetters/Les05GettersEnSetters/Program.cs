b1c34b77f7dfc9a948b3c132014c76403611318d
10KJVN <Jahva_game11@hotmail.com>
2022-04-14 12:44:25 +0200

    Getters en setters halverwege

---
+++ Solution01/Les05/Les05GettersEnSetters/Les05GettersEnSetters/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Les05GettersEnSetters
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            var dog = new dog("MyDawg");
+            dog.pet();
+            var age = dog.getAge();
+        }
+    }
+}
