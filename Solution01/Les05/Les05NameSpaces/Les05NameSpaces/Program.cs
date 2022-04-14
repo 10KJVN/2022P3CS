9bd48f81f182b9c4454a9d9532808809a4550ba6
10KJVN <Jahva_game11@hotmail.com>
2022-04-14 13:56:37 +0200

    Les6 en de laatse van 5 inhalen, de rest af

---
+++ Solution01/Les05/Les05NameSpaces/Les05NameSpaces/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Les05NameSpaces;
+
+namespace Les05NameSpaces
+{
+    class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Test functie");
+        }
+    }
+}
+
+namespace Les05NameSpaces2
+{
+    class ProgramV2
+    {
+        public void
+            {
+              //Geen idee hoe dit werkt, deze haal ik in
+            }
+            
+    }
+}
---
+++ Solution01/Les05/Les06Overerving/Les06Overerving/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Les06Overerving
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Deze en Namespaces haal ik in. gaat niet lukken binnen de deadline.");
+        }
+    }
+}
