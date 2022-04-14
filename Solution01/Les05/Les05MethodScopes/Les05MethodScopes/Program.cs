6c5e1a366596d7b8a6f27eed96ab109168d0d1f4
10KJVN <Jahva_game11@hotmail.com>
2022-04-14 10:48:27 +0200

    Scopes en method scopes af

--- Solution01/Les05/Les05MethodScopes/Les05MethodScopes/Program.cs
+++ Solution01/Les05/Les05MethodScopes/Les05MethodScopes/Program.cs
@@ -4,9 +4,33 @@ namespace Les05MethodScopes
 {
     internal class Program
     {
-        static void Main(string[] args)
+
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (true)
+            {
+                // Scope line 8: If Statement & Alles er buiten
+                string scope8Var = "8";
+
+                string scope12Var = "N/A";
+                if (true)//Deze regel mag niet worden veranderd
+                {
+                    // Scope line 12: If Statement & Alles er buiten
+                    scope12Var = "12";
+                }
+
+                string scope17Var = "N/A";
+                if (false)//Deze regel mag niet worden veranderd
+                {
+                    // Scope line 17: If Statement & Alles er buiten    
+                    scope17Var = "17";
+                }
+
+                //Code hieronder moet hier blijven staan. en mag niet aangepast worden
+                Console.WriteLine("This was set in scope line 8: " + scope8Var);
+                Console.WriteLine("This was set in scope line 12: " + scope12Var);
+                Console.WriteLine("This was set in scope line 17: " + scope17Var);
+            }
         }
     }
 }
---
+++ Solution01/Les05/Les05Scopes/Les05Scopes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Les05Scopes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            NewScope();
+        }
+
+        public static void NewScope()
+        {
+            Console.WriteLine("Let op dit is een nieuwe scope!");
+            Console.WriteLine("Is dit een nieuwe scope?");
+            float RandomVariable = 6.197f;
+            Console.WriteLine(RandomVariable);
+        }
+
+    }
+}
