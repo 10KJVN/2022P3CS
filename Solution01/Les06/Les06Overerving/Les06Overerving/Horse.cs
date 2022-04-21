67804cc8d87f7b9e355397ea7b42bdf4c33e6807
10KJVN <Jahva_game11@hotmail.com>
2022-04-21 11:11:59 +0200

    Horse zou nu noeten erfen

--- Solution01/Les06/Les06Overerving/Les06Overerving/Horse.cs
+++ Solution01/Les06/Les06Overerving/Les06Overerving/Horse.cs
@@ -6,11 +6,8 @@ using System.Threading.Tasks;
 
 namespace Les06Overerving
 {
-    internal class Horse
+    internal class Horse : Animal
     {
-        public void Feed()
-        {
 
-        }
     }
 }
